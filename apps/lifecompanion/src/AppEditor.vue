b2eb08cc58c4ee5c836123416ec0df2fd18b97d0
OscarGitH <pavoinepro@gmail.com>
2026-08-06 15:53:36 +0200

    feat: wip update

--- apps/lifecompanion/src/AppEditor.vue
+++ apps/lifecompanion/src/AppEditor.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { check } from "@tauri-apps/plugin-updater";
 import LcSnackbarQueue from "./components/shared/vuetify/LcSnackbarQueue.vue";
 import ConfigEditorView from "./screens/editor/ConfigEditorView.vue";
 import CreateOrLoadConfigView from "./screens/editor/CreateOrLoadConfigView.vue";
 import { usePlayerStore } from "./stores/player/usePlayerStore";
 
 const playerStore = usePlayerStore();
+
+const update = await check();
+
+console.log(update);
 </script>
 
 <template>
